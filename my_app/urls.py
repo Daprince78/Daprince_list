583046829e2d4e6794eaf5d59b2999a30d2fe431
Daprince78 <azubuikenaza@gmail.com>
2020-08-30 19:47:34 +0100

    My Daprince_list app

---
+++ my_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search'),
+]
+
+# ways to refer to code urls
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
